e546e876bd46ec2cfbd09d3da982b73fcfc99ca2
Naufal Fawwazi <naufalfawwazi0@gmail,com>
2024-06-19 01:04:46 +0700

    menambahkan gambar

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <link rel="icon" type="image/x-icon" href="images/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/x-icon" href="images/logo.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <title>Check Khodam</title>
     <style>
         body {
@@ -14,6 +14,7 @@
             align-items: center;
             height: 100vh;
             margin: 0;
+            background: linear-gradient(160deg, #2b07f7, #f30909);
         }
         .container {
             background-color: #f0f0f0;
@@ -64,29 +65,48 @@
             <label for="inputName">Nama:</label>
             <input type="text" id="inputName" name="inputName" required>
             <br>
-            <button type="submit">Check Khodam</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <br><br>
+            <button type="button"  data-bs-toggle="modal" data-bs-target="#detailModal">Cek khodam</button>
         </form>
-        
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-            <div class="result">
-            <?php
+        <?php
             // Sisipkan file logika PHP
             require_once 'qodam.php';
- // Proses input dan tampilkan hasil jika form disubmit
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $inputName = isset($_POST['inputName']) ? htmlspecialchars($_POST['inputName']) : '';
-    $khodamName = getRandomqodam();
-    echo "<p><strong>Nama:</strong> $inputName</p>";
-    echo "<p><strong>Khodam:</strong> $khodamName</p>";
-}
-?>
-</div>
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Kembali</button>
+            // Proses input dan tampilkan hasil jika form disubmit
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $inputName = isset($_POST['inputName']) ? htmlspecialchars($_POST['inputName']) : '';
+                $khodamName = getRandomQodam();
+            }
+            ?>
+        <div class="modal fade" id="detailModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Khodam kamu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+    <div class="modal-body row justify-content-center align-items-center" >
+        <div class="card " style="width: 18rem;">
+            <img src="img/cek.png" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title"><?php echo $khodamName; ?></h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, non ducimus doloremque sequi dolorum. Fugiat quidem soluta veniam vitae mollitia dolorum!</p>
         </div>
     </div>
+        <div class="result">
+            <?php
+            echo "<p><strong>Nama:</strong> $inputName</p>";
+            echo "<p><strong>Khodam:</strong> $khodamName</p>";?>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Kembali</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
